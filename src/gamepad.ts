9debaab219ac988fc00d3da1ea3fff91cd803081
Valentin Degenne <vdegenne@gmail.com>
2026-02-17 16:56:59 +0100

    pointerlock

--- src/gamepad.ts
+++ src/gamepad.ts
@@ -14,6 +14,7 @@ class GamepadController extends ReactiveController {
 			focusDeadTimeMs: 200,
 		})
 		minigp.onConnect((gamepad) => {
+			document.body.requestPointerLock()
 			this.gamepad = gamepad
 			const map = gamepad.mapping
 
